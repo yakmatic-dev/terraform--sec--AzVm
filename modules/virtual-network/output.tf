a3f65a6db6f226ec7b4db5ae8ef43bb24feb997d
yakmatic <yakmatic1@gmail.com>
2025-11-16 08:13:29 +0100

    outputing the subnet

--- modules/virtual-network/output.tf
+++ modules/virtual-network/output.tf
@@ -1,1 +1,9 @@
-
+output "alpha_address_space" {
+  value = local.alpha_address_space
+}
+output "beta_address_space" {
+  value = local.beta_address_space
+}
+output "name" {
+  value = azurerm_virtual_network.main.name
+}
