62870888d9103093733671bb82201419c69fc402
yakmatic-dev <yakmatic1@gmail.com>
2025-11-16 11:44:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "nsg" {
       direction                  = "Inbound"
       access                     = "Allow"
       protocol                   = "Tcp"
-      source_port_range          = ""
+      source_port_range          = "*"
       destination_port_range     = "22"
       source_address_prefix      = "*"
       destination_address_prefix = "*"
