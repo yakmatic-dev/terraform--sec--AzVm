b2e052a7c8ea4edaac51837278fe1dcae8485993
yakmatic <yakmatic1@gmail.com>
2025-11-16 08:12:47 +0100

    defining the subnet variable

---
+++ modules/subnet/variable.tf
@@ -0,0 +1,24 @@
+variable "application" {
+  description = "app name"
+  type        = string
+}
+
+variable "environment" {
+  description = "env name"
+  type        = string
+}
+
+variable "rg_name" {
+  description = "Resource Group name where the Subnet will be created"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Virtual Network name where the Subnet will be created"
+  type        = string
+}
+
+variable "local_alpha_address_space" {
+  description = "Local alpha variable for address spaces"
+  type        = string
+}
