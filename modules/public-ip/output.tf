40b7685d190d2c663b17b5d3d700ddc1c40b7d57
yakmatic <yakmatic1@gmail.com>
2025-11-17 09:02:06 +0100

    modifying output file

--- modules/public-ip/output.tf
+++ modules/public-ip/output.tf
@@ -1,3 +1,9 @@
-output "public_ip" {
-  value = azurerm_public_ip.vm1.ip_address
+output "id" {
+  description = "The ID of the public IP"
+  value       = azurerm_public_ip.vm1.id
+}
+
+output "ip_address" {
+  description = "The allocated public IP address"
+  value       = azurerm_public_ip.vm1.ip_address
 }
