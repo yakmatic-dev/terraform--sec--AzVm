f9f39b10924e8ecadf55dbaad4e64f25c1892600
yakmatic <yakmatic1@gmail.com>
2025-11-20 04:10:24 +0100

    kv modules

---
+++ modules/kv/main.tf
@@ -0,0 +1,22 @@
+resource "random_string" "kv_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+data "azurerm_client_config" "current" {}
+
+
+resource "azurerm_key_vault" "main" {
+  name                       = "kv-linuxvm-${random_string.kv_suffix.result}"
+  location                   = var.location
+  resource_group_name        = var.rg_name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = var.sku_name                   #"standard"
+  soft_delete_retention_days = var.soft_delete_retention_days #7
+  purge_protection_enabled   = var.purge_protection_enabled   #false
+
+  rbac_authorization_enabled = var.rbac_authorization_enabled #true
+}
+
+
---
+++ modules/kv/variable.tf
@@ -0,0 +1,40 @@
+variable "rg_name" {
+  description = "Resource Group name"
+  type        = string
+
+}
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+variable "application" {
+  description = "app name"
+  type        = string
+}
+variable "environment" {
+  description = "env name"
+  type        = string
+}
+
+
+
+variable "sku_name" {
+  description = "The SKU name of the Key Vault"
+  type        = string
+  default     = "standard"
+}
+variable "soft_delete_retention_days" {
+  description = "The number of days that deleted Key Vaults are retained"
+  type        = number
+  default     = 7
+}
+variable "purge_protection_enabled" {
+  description = "Specifies whether purge protection is enabled for this Key Vault"
+  type        = bool
+  default     = false
+}
+variable "rbac_authorization_enabled" {
+  description = "Specifies whether RBAC authorization is enabled for this Key Vault"
+  type        = bool
+  default     = true
+}
