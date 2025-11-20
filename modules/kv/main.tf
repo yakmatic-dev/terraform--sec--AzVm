2653ecb4ba01664db4b146396188219594f10d7d
yakmatic <yakmatic1@gmail.com>
2025-11-20 13:18:31 +0100

    updating it to have rbac

--- modules/kv/main.tf
+++ modules/kv/main.tf
@@ -20,3 +20,9 @@ resource "azurerm_key_vault" "main" {
 }
 
 
+resource "azurerm_role_assignment" "kv_admin" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
